fcc1ee7e6ae9e2620b848beb788a1758db9ad034
Tran Manh Duc <manhduc1907@gmail.com>
2026-03-26 23:19:17 +0700

    create new user

---
+++ Object Repository/POST a new user.rs
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>POST a new user</name>
+   <tag></tag>
+   <elementGuidId>38dd6c7f-ab86-497d-827d-74af558dabb7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>false</autoUpdateContent>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>true</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n  \&quot;age\&quot;: ${age},\n  \&quot;avatar\&quot;: null,\n  \&quot;gender\&quot;: \&quot;${gender}\&quot;,\n  \&quot;password\&quot;: \&quot;${password}\&quot;,\n  \&quot;username\&quot;: \&quot;${username}\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>3201fff9-c8f0-4afc-a00e-19c7618571fe</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>10.4.3</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <path></path>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://sample-web-service-aut.herokuapp.com/api/users/json</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>18</defaultValue>
+      <description></description>
+      <id>b6e3fbc7-190c-44a2-9132-9fef697b6a95</id>
+      <masked>false</masked>
+      <name>age</name>
+   </variables>
+   <variables>
+      <defaultValue>'MALE'</defaultValue>
+      <description></description>
+      <id>7b1d0cf3-d388-4c1d-8ddd-c500eaf665c3</id>
+      <masked>false</masked>
+      <name>gender</name>
+   </variables>
+   <variables>
+      <defaultValue>'mimi'</defaultValue>
+      <description></description>
+      <id>29fc6285-de45-4a4a-b06c-902895be3391</id>
+      <masked>false</masked>
+      <name>username</name>
+   </variables>
+   <variables>
+      <defaultValue>'123456789'</defaultValue>
+      <description></description>
+      <id>a0be81df-a2cf-4b12-879c-acbd80cbcc6c</id>
+      <masked>false</masked>
+      <name>password</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+assert response.getStatusCode() == 200
+WS.verifyElementPropertyValue(response, 'age', 18)
+WS.verifyElementPropertyValue(response, 'username', 'mimi')
+WS.verifyElementPropertyValue(response, 'password', '123456789')
+WS.verifyElementPropertyValue(response, 'gender', 'MALE')</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
