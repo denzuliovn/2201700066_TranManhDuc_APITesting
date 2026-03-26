0ba0bfec674c7745ba55af31c895cde0d18c054b
Tran Manh Duc <manhduc1907@gmail.com>
2026-03-26 23:18:49 +0700

    get user by id

---
+++ Object Repository/GET user by id.rs
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>GET user by id</name>
+   <tag></tag>
+   <elementGuidId>285128c1-d642-41d0-96e3-885d0d8d59aa</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>true</autoUpdateContent>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>true</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>10.4.3</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <path></path>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://sample-web-service-aut.herokuapp.com/api/users/${id}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>117</defaultValue>
+      <description></description>
+      <id>81e0b9bb-fdee-4c15-bb1f-741c55301986</id>
+      <masked>false</masked>
+      <name>id</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+assert response.getStatusCode() == 200
+WS.verifyElementPropertyValue(response, 'id', id)
+WS.verifyElementPropertyValue(response, 'username', 'mimi')
+WS.verifyElementPropertyValue(response, 'gender', 'MALE')
+WS.verifyElementPropertyValue(response, 'age', 18)</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
